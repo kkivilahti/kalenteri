83db42aaaa4e24a3bdc18516c7240f978521e463
kkivilahti <159913043+kkivilahti@users.noreply.github.com>
2024-04-20 13:17:09 +0000

    .

---
+++ kesakalenteri.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById("myModal");
+
+        let span = document.getElementsByClassName("close")[0];
+
+        function vko1(){
+            modal.style.display = "block";
+        }
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
