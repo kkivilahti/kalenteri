e6b26d25edc2d5d628bdb8f2526412371c2d2b78
kkivilahti <159913043+kkivilahti@users.noreply.github.com>
2024-04-20 13:46:33 +0000

    .

--- kesakalenteri.js
+++ kesakalenteri.js
@@ -1,10 +1,10 @@
 let modal = document.getElementById("myModal");
 
-        let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
-        function vko1(){
-            modal.style.display = "block";
-        }
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
+function vko1(){
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
