6ef5275844fd3f956c66d2729213816a69fdce47
kkivilahti <159913043+kkivilahti@users.noreply.github.com>
2024-04-24 10:34:07 +0000

    .

--- kesakalenteri.js
+++ kesakalenteri.js
@@ -1,10 +1,30 @@
 let modal = document.getElementById("myModal");
+let modal1 = document.getElementById("myModal1");
 
-let span = document.getElementsByClassName("close")[0];
-
+// Avaa pop upin
 function vko1(){
     modal.style.display = "block";
 }
+function vko2(){
+    modal1.style.display = "block";
+}
+
+
+let span = document.getElementsByClassName("close")[0];
+let span1 = document.getElementsByClassName("close1")[0];
+
+// Sulkee pop upin x-painikkeesta
 span.onclick = function() {
     modal.style.display = "none";
+}
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+// Sulkee klikatessa pop upin ulkopuolelle
+window.onclick = function(event) {
+if (event.target == modal || event.target == modal1) {
+      modal.style.display = "none";
+      modal1.style.display = "none";
+    }
 }
